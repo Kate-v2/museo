3a13803677c6cf79b8b34c2e0de1a942afc75011
Katelyn <katelyn.rimkunas@gmail.com>
2018-09-18 09:17:55 -0600

    Iteration one complete - Artist & Photograph

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+
+
+
+
+
+class Artist
+
+  attr_reader :id, :name, :born, :died, :country
+
+  def initialize(hash)
+    @id      = hash[:id]
+    @name    = hash[:name]
+    @born    = hash[:born]
+    @died    = hash[:died]
+    @country = hash[:country]
+  end
+
+  
+
+
+
+end
